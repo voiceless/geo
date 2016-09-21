69494fd2ce58218f94078c612636bb4aaa44a8ca
pbezglasnyi <pavelb23@gmail.com>
2016-09-21 14:33:33 +0300

    Add random generator

--- src/main/scala-2.11/generator/GeneratorMain.scala
+++ src/main/scala-2.11/generator/GeneratorMain.scala
@@ -1,12 +1,35 @@
 package generator
 
+import model.{Grid, Label}
+
+import scala.reflect.io.File
+import scala.util.Random
+
 /**
   * Created by pbezglasnyi on 20.09.2016.
   */
 object GeneratorMain extends App {
 
-  def generateLabel(size: Int) = {
+  val lebels = File(args(0)).createFile()
 
+  val grids = File(args(1)).createFile()
+
+  val random = new Random
+
+  generateLabel(100, lebels)
+  generateGrid(100, grids)
+
+  def generateLabel(size: Int, file: File) {
+    val writer = file.printWriter()
+    writer.println("user_id,lon,lat")
+    (1 to size).toStream.map(i => Label(i, random.nextDouble() * 1000, random.nextDouble() * 1000)).foreach(l => writer.println(l.toCsvString))
   }
+
+  def generateGrid(size: Int, file: File) {
+    val writer = file.printWriter()
+    writer.println("tile_x,tile_y,distance_error")
+    (1 to size).toStream.map(i => Grid(random.nextInt(), random.nextInt(), random.nextDouble() * 10)).foreach(g => writer.println(g.toScvString))
+  }
+
 
 }
--- src/main/scala-2.11/model/Model.scala
+++ src/main/scala-2.11/model/Model.scala
@@ -3,6 +3,10 @@ package model
 /**
   * Created by pbezglasnyi on 20.09.2016.
   */
-case class Label(user_id: Int, lon: Double, lat: Double)
+case class Label(user_id: Int, lon: Double, lat: Double) {
+  def toCsvString = s"$user_id,$lon,$lat"
+}
 
-case class Grid(tile_x: Int, tile_y: Int, distance_error: Double)
+case class Grid(tile_x: Int, tile_y: Int, distance_error: Double) {
+  def toScvString = s"$tile_x,$tile_y,$distance_error"
+}
